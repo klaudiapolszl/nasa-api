83ffaddde8826b8dc1d44d0452ba289baf6b6839
Klaudia <k.polan95@gmail.com>
2020-03-16 00:15:46 +0100

    Updates

---
+++ precache-manifest.9aead69149895650abd72095ebebcbf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7013fb2ba814e1976535486153798f",
+    "url": "/nasa-api/index.html"
+  },
+  {
+    "revision": "d91b11f9475399125afa",
+    "url": "/nasa-api/static/css/main.1aa27d6c.chunk.css"
+  },
+  {
+    "revision": "05f95ab53edf42f4dee9",
+    "url": "/nasa-api/static/js/2.298d2f19.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/nasa-api/static/js/2.298d2f19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d91b11f9475399125afa",
+    "url": "/nasa-api/static/js/main.ec2487a5.chunk.js"
+  },
+  {
+    "revision": "9a0e65d507cd57449536",
+    "url": "/nasa-api/static/js/runtime-main.76776cf5.js"
+  }
+]);
