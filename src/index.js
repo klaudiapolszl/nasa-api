4db7dd5deab7c48fb0638ceac55d885fac2e1988
Klaudia <k.polan95@gmail.com>
2020-05-07 23:09:36 +0200

    Rename classes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './style.scss';
 import './responsive.scss';
 
-class PicOfTheDay extends React.Component { 
+class PicOfTheDay extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             loading: true,
             title: '',
             explanation: '',
@@ -22,7 +22,7 @@ class PicOfTheDay extends React.Component {
         fetch(`https://api.nasa.gov/planetary/apod?api_key=NMnY548zyrPfYSxvM1mATNzhwYbOqgEkFiwV422Z`)
             .then(r => r.json())
             .then(ans => {
-                this.setState({ 
+                this.setState({
                     title: ans.title,
                     explanation: ans.explanation,
                     date: ans.date,
@@ -36,7 +36,7 @@ class PicOfTheDay extends React.Component {
     }
 
     render() {
-        if (this.state.loading) { 
+        if (this.state.loading) {
             return (
                 <div className="loading">
                     Please, wait a moment...
@@ -48,17 +48,17 @@ class PicOfTheDay extends React.Component {
                 <h1 className="main-title">
                     Astronomy Picture of the Day
                 </h1>
-                <p className="pic-title text">
+                <p className="media-title text">
                     { this.state.title }
                 </p>
-                <p className="pic-date text">
+                <p className="media-date text">
                     { this.state.date }
                 </p>
                 { (this.state.copyright != null) ? <p className="text">Copyright: { this.state.copyright }</p> : "" }
                 <div className="polaroid">
-                    { (this.state.media_type === "video") ? <iframe src={ this.state.video }></iframe> : <img src={ this.state.img } alt="Image from NASA Library"></img> }
-                    <div className="text-container">
-                        <p className="pic-description">
+                    { (this.state.media_type === "video") ? <iframe src={ this.state.video } title={ this.state.title } className="polaroid__media"></iframe> : <img src={ this.state.img } alt={ this.state.title } className="polaroid__media"></img> }
+                    <div className="polaroid__text-container">
+                        <p className="polaroid__media-desc">
                             { this.state.explanation }
                         </p>
                     </div>
